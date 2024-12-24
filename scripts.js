b0a222fe18846f49bd59134038ffa27d37334ec3
Jafar Aziz Shariff Mohammad <75298878+Jafar-97@users.noreply.github.com>
2024-12-24 12:58:45 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
   const navbar = document.querySelector('.navbar');
-  const footer = document.querySelector('footer');
   navbar.classList.toggle('dark-mode');
-  footer.classList.toggle('dark-mode');
 }
